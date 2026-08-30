ae1d61d009f848b008eed610f497d6199ecb851d
m2panda <garciaisaac468@gmail.com>
2026-08-30 13:24:14 -0600

    style: :gem: add prettier config and ignore file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
    vite: {
-      plugins: [tailwindcss()],
+      plugins: [
+         tailwindcss(),
+         {
+            name: '@astrojs/ts-plugin',
+         },
+      ],
    },
 });
