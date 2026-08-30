9dc8e3f20f00cdb14ac227122e12377a231411d0
m2panda <garciaisaac468@gmail.com>
2026-08-30 12:42:39 -0600

    feat: :sparkles: add footer to index page
    - add code to footer
    - removed github pages deployment workflow
    - add about, contact and examples components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
    vite: {
       plugins: [tailwindcss()],
    },
-   site: 'https://m2panda.github.io',
-   base: '/dgrade-demo'
 });
