7d22eaf13e319298845f868f07e6ebf76d01de8c
m2panda <garciaisaac468@gmail.com>
2026-09-04 15:01:33 -0600

    style: :gem: change css font face to astro custom fonts
    - remove fonts.css file and import
    - add custom fonts to astro.config.mjs
    - add font import on index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +12,49 @@ export default defineConfig({
          },
       ],
    },
+   fonts: [
+      {
+         provider: fontProviders.local(),
+         name: 'Roboto',
+         cssVariable: '--custom-font-roboto',
+         options: {
+            variants: [
+               {
+                  src: ['./src/assets/fonts/roboto/static/Roboto-Regular.ttf'],
+                  weight: 400,
+                  style: 'normal',
+               },
+               {
+                  src: ['./src/assets/fonts/roboto/static/Roboto-Italic.ttf'],
+                  weight: 400,
+                  style: 'italic',
+               },
+               {
+                  src: ['./src/assets/fonts/roboto/static/Roboto-Bold.ttf'],
+                  weight: 700,
+                  style: 'normal',
+               },
+               {
+                  src: [
+                     './src/assets/fonts/roboto/static/Roboto-BoldItalic.ttf',
+                  ],
+                  weight: 700,
+                  style: 'italic',
+               },
+               {
+                  src: ['./src/assets/fonts/roboto/static/Roboto-Light.ttf'],
+                  weight: 300,
+                  style: 'normal',
+               },
+               {
+                  src: [
+                     './src/assets/fonts/roboto/static/Roboto-LightItalic.ttf',
+                  ],
+                  weight: 300,
+                  style: 'italic',
+               },
+            ],
+         },
+      },
+   ],
 });
